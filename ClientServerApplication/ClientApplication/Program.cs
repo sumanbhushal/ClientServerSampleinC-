e7ba6cf7b86641794c8f9fd09c2f19f5e8c05e90
sumanbhushal <sumanbhushal@gmail.com>
2016-03-14 15:08:15 +0100

    First Prototype

---
+++ ClientServerApplication/ClientApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            string server = "127.0.0.1";
+            var port = 51234;
+
+            Console.WriteLine("Enter the text to Send to the Server for changing it to upper case: ");
+            string message = Console.ReadLine();
+
+            //initializaing the tcpclient;
+            TcpClient myClient = new TcpClient(server, port);
+            //Translate the passing message into ASCII and store it in byte
+            byte[] msgData = System.Text.Encoding.ASCII.GetBytes(message);
+
+            //geting client stream to read and write
+            NetworkStream clientStream = myClient.GetStream();
+
+            //sending the message to TCPServer
+            clientStream.Write(msgData, 0, msgData.Length);
+            Console.WriteLine("Sent Message: {0}", message);
+
+            //Receiving TCPServer Response
+
+            //buffer to store the response
+            msgData = new byte[256];
+
+            //string to store the response ASCII representation
+            string responseMsg = string.Empty;
+
+            //Read the first byte of tcpresponse
+            int byteMsg = clientStream.Read(msgData, 0, msgData.Length);
+            responseMsg = System.Text.Encoding.ASCII.GetString(msgData, 0, byteMsg);
+            Console.WriteLine("Received : {0}", responseMsg);
+
+            clientStream.Close();
+
+            myClient.Close();
+        }
+    }
+}
---
+++ ClientServerApplication/ServerApplication/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setting the TCP listerner in port 51234
+            int port = 51234;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpServer = null;
+            try
+            {
+
+                tcpServer = new TcpListener(ipAddress, port);
+
+                //starting listerning client
+                tcpServer.Start();
+
+                //Buffer for reading data from client
+                byte[] bytes = new byte[256];
+                string data = null;
+                string dataToUpper = null;
+
+
+                //listening loop
+                while (true)
+                {
+                    Console.WriteLine("Waiting for the Connecton");
+
+                    //Call to accept request
+                    TcpClient clientSystem = tcpServer.AcceptTcpClient();
+                    Console.WriteLine("Connected !!!");
+
+
+                    //getting a stream object for reading and writing 
+                    NetworkStream dataStream = clientSystem.GetStream();
+
+                    int i;
+                    //loop to receive all the data send by the client
+                    while ((i = dataStream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        //Translate data byte into ASCII value
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received Data : {0}", data);
+
+                        //process the data to upper case
+                        dataToUpper = data.ToUpper().ToString();
+                        Console.WriteLine("The output is: ", dataToUpper);
+
+                        byte[] respnseMsg = System.Text.Encoding.ASCII.GetBytes(dataToUpper);
+
+                        //sending back the response 
+                        dataStream.Write(respnseMsg, 0, respnseMsg.Length);
+                        Console.WriteLine("Send {0}", dataToUpper);
+                    }
+
+                    //shutdown and end the connection
+                    clientSystem.Close();
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Socket Exception : {0} ", e);
+            }
+            finally
+            {
+                //stop listening for new client
+                tcpServer.Stop();
+            }
+        }
+    }
+}
